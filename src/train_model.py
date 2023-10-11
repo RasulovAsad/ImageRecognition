a529a58efe6a68237501c1db79841cc891f13977
RasulovAsad <asadrasulov00@gmail.com>
2023-10-11 15:04:48 +0400

    script update

--- src/train_model.py
+++ src/train_model.py
@@ -6,12 +6,16 @@ from keras.applications import ResNet50
 from keras.layers import GlobalAveragePooling2D, Dense
 from keras.models import Model
 from keras.optimizers import Adam
+from keras.utils import to_categorical
 from keras.preprocessing.image import ImageDataGenerator
+from sklearn.preprocessing import LabelEncoder
+from keras.utils import to_categorical
 
 num_classes = 5
 
 # Предобработка изображений
 def load_images_from_folder(folder):
+    print('loading images')
     images = []
     labels = []
     for subfolder in os.listdir(folder):
@@ -38,6 +42,16 @@ preprocessed_images = preprocess_images(images)
 print('Разделение данных на обучающую и проверочную выборки')
 X_train, X_val, y_train, y_val = train_test_split(preprocessed_images, labels, test_size=0.2, random_state=42)
 
+
+# Преобразование строковых меток в целые числа
+label_encoder = LabelEncoder()
+y_train_encoded = label_encoder.fit_transform(y_train)
+y_val_encoded = label_encoder.transform(y_val)
+
+# Преобразование целых чисел в one-hot кодировку
+y_train_onehot = to_categorical(y_train_encoded, num_classes)
+y_val_onehot = to_categorical(y_val_encoded, num_classes)
+
 # Создание модели
 print('Создание модели')
 base_model = ResNet50(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
@@ -48,7 +62,9 @@ predictions = Dense(num_classes, activation='softmax')(x)
 model = Model(inputs=base_model.input, outputs=predictions)
 model.compile(optimizer=Adam(learning_rate=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
 
-# Создайте генераторы данных для обучения и проверки
+
+
+# Создание генераторов данных для обучения и проверки
 train_datagen = ImageDataGenerator(
     rescale=1.0/255.0,  # Масштабирование значений пикселей
     rotation_range=20,  # Вращение изображений для аугментации данных (по желанию)
@@ -60,16 +76,16 @@ train_datagen = ImageDataGenerator(
 
 val_datagen = ImageDataGenerator(rescale=1.0/255.0)  # Только масштабирование для проверочных данных
 
-# Создайте генераторы для обучающих и проверочных данных
+# Создание генераторов для обучающих и проверочных данных
 batch_size = 32
 
 train_generator = train_datagen.flow(
-    X_train, y_train,
+    X_train, y_train_onehot,  # Используйте y_train_onehot
     batch_size=batch_size
 )
 
 val_generator = val_datagen.flow(
-    X_val, y_val,
+    X_val, y_val_onehot,  # Используйте y_val_onehot
     batch_size=batch_size
 )
 
@@ -82,6 +98,10 @@ history = model.fit(
     validation_data=val_generator  # Генератор проверочных данных
 )
 
+print('Сохранение модели')
+model.save('my_model.keras')
+
+
 # # Обучение модели
 # print('Обучение модели')
 # num_epochs = 10
@@ -91,5 +111,3 @@ history = model.fit(
 #     epochs=num_epochs,
 #     validation_data=(X_val, y_val)
 # )
-print('Сохранение модели')
-model.save('my_model.h5')
